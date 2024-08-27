477672c8db092073c264234ad5e6ec83a17ad9cf
must-Zeus0036 <mustafa.al-bayati0036@stud.hkr.se>
2024-08-27 22:06:36 +0200

    update player.py

--- palyer.py
+++ palyer.py
@@ -10,3 +10,17 @@ class Player:
     def reset_turn_score(self):
         """Reset the turn score to zero."""
         self.turn_score = 0
+
+    def add_turn_score(self, score):
+        """Add the score to the player's turn score."""
+        self.turn_score += score
+
+    def add_total_score(self):
+        """Add the turn score to the total score and reset turn score."""
+        self.total_score += self.turn_score
+        self.reset_turn_score()
+
+    def get_total_score(self):
+        """Return the player's total score."""
+        return self.total_score
+
