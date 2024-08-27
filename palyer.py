5bb37734f992f8fd3f241cb82df8ee94a17eb272
must-Zeus0036 <mustafa.al-bayati0036@stud.hkr.se>
2024-08-27 22:04:26 +0200

    commit player.py

---
+++ palyer.py
@@ -0,0 +1,10 @@
+
+class Player:
+    """Class representing a player in the game."""
+
+    def __init__(self, name):
+        self.name = name
+        self.total_score = 0
+        self.turn_score = 0
+
+
